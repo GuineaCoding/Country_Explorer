6b1078345c9fc79292246f0047e61548317a6e5f
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-02-11 23:33:48 +0000

    Implemented on click category, to redirect to it's page

--- src/controllers/landmark-category-controller.js
+++ src/controllers/landmark-category-controller.js
@@ -1,14 +1,43 @@
 import { TrackSpec } from "../models/joi-schemas.js";
 import { db } from "../models/db.js";
+import { getDatabase, ref, get, push, remove } from "firebase/database";
+import { LandmarkCategorySpec } from "../models/joi-schemas.js";
 
 export const landmarkCategoryController = {
   index: {
     handler: async function (request, h) {
-      const landmarkCategory = await db.landmarkCategoryStore.getLandmarkCategoryById(request.params.id);
+      console.log("Accessing landmarkCategoryController.index handler");
+      const categoryId = request.params.id;
+      console.log(`Received category ID: ${categoryId}`);
+      const firebaseDB = getDatabase();
+      const userEmail = request.auth.credentials.email.replace(/\./g, ',');
+      console.log(`Sanitized user email: ${userEmail}`);
+      let landmarkCategory;
+
+      try {
+        console.log(`Attempting to retrieve landmark category with ID: ${categoryId} from Firebase`);
+        const landmarkCategoryRef = ref(firebaseDB, `users/${userEmail}/landmarkCategories/${categoryId}`);
+        const landmarkCategorySnap = await get(landmarkCategoryRef);
+
+        if (landmarkCategorySnap.exists()) {
+          landmarkCategory = landmarkCategorySnap.val();
+          console.log(`Landmark category data retrieved: ${JSON.stringify(landmarkCategory)}`);
+          landmarkCategory._id = categoryId; 
+        } else {
+          console.log(`No landmark category found with ID: ${categoryId}`);
+          return h.response("Not Found").code(404);
+        }
+      } catch (error) {
+        console.error(`Error retrieving landmark category with ID ${categoryId} from Firebase:`, error);
+        return h.response("An internal server error occurred").code(500);
+      }
+
       const viewData = {
         title: "Landmark Category",
         landmarkCategory: landmarkCategory,
       };
+
+      console.log(`Rendering view with data: ${JSON.stringify(viewData)}`);
       return h.view("landmarkCategory-view", viewData);
     },
   },
